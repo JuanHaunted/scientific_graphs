8ed9d583337696aa1fc0b88c0dd72c8ebe61b5f9
juanca <juankospina0530@gmail.com>
2023-02-20 16:02:52 -0500

    Solve wrong values for refraction angles

--- abbe_number.py
+++ abbe_number.py
@@ -1,20 +1,32 @@
 #Calculamos refraction index
 from functions import *
 
-n_violet = get_refraction_angle(48.53)
-n_green = get_refraction_angle(47.55)
-n_yellow = get_refraction_angle(46.68)
-n_red = get_refraction_angle(46.13)
+#n_violet = get_refraction_angle(48.53)
+#n_green = get_refraction_angle(47.55)
+#n_yellow = get_refraction_angle(46.68)
+#n_red = get_refraction_angle(46.13)
+
+n_violet = get_refraction_angle(56.53)
+n_green = get_refraction_angle(55.55)
+n_yellow = get_refraction_angle(54.68)
+n_red = get_refraction_angle(54.13)
+
 
 print(n_violet, n_green, n_yellow, n_red)
 
-n_arr = [n_green, n_yellow, n_red]
+#443,755
+n_arr = [n_violet, n_yellow, n_red]
 
-cauchy_aproximation = get_cauchy_aprox(501.567, 587.562, 667.815, n_arr)
+cauchy_aproximation = get_cauchy_aprox(443.755, 587.562, 667.815, n_arr)
 
-n_d = cauchy_aproximation(466.814)
-n_F = cauchy_aproximation(486.134)
-n_C = cauchy_aproximation(656.281)
+n_d = cauchy_aproximation(587.6)
+n_F = cauchy_aproximation(486.1)
+n_C = cauchy_aproximation(656.3)
+
+print(n_d, n_F, n_C)
+
 
 abbe_number = calc_abbe_number(n_d, n_F, n_C)
 print(abbe_number)
+
+
