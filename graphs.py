29796ebb16dddc1a153c6941159fb789d54283cb
juanca <juankospina0530@gmail.com>
2023-02-19 21:51:08 -0500

    Add graphs

---
+++ graphs.py
@@ -0,0 +1,50 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from functions import *
+
+
+
+degrees = [318.25, 317.37, 316.83, 316.62 ,315.15 ,314.3 ,313.72]
+wavelenghts = [443.755, 471.314, 492.193, 501.567, 587.562, 667.815, 686.7]
+eq = lambda x:6.0331*(x**2) - 3869.4*x + 620821
+np.vectorize(eq)
+x_axis_inter = np.linspace(313, 318.5, 100)
+interpolation = eq(x_axis_inter)
+
+
+
+fig, ax = plt.subplots(figsize=(5, 3))
+
+ax.set_title('Curva de Calibración del Helio')
+ax.set_ylabel('Longitud de Onda λ (nm)')
+ax.set_xlabel('Grados (°)')
+ax.scatter(degrees, wavelenghts)
+ax.plot(x_axis_inter, interpolation, '--', color='orange', label=r'$6.0331x^{2} - 3869.4x + 620821$')
+ax.text(317, 650, r'$r^{2} = 0.9924$', fontsize = 10)
+ax.legend(loc='upper right')
+ax.grid()
+fig.tight_layout()
+
+
+delta = np.array([53.38, 49.32, 46.55, 45.53, 45.17, 46.73, 47.65, 49.92, 50.8, 51.12])
+theta_i = np.array([40.25, 42.25, 43.75, 45.25, 47.75, 60.75, 61.25, 67, 68, 69.25])
+eq2 = lambda x: 0.0345*(x**2) - 3.7344*x + 145.53
+np.vectorize(eq2)
+theta_axis_inter = np.linspace(40, 70, 10)
+delta_inter = eq2(theta_axis_inter)
+
+print(len(theta_i), len(delta))
+fig1, ax1 = plt.subplots(figsize=(5, 3))
+
+ax1.set_title(r'Desviación Mínima $\delta$  v.s Ángulo de Incidencia $\theta_i$')
+ax1.set_ylabel(r'Desviación $\delta$ (°)')
+ax1.set_xlabel(r'Ángulo de Incidencia $\theta_i$ (°)')
+ax1.scatter(theta_i, delta)
+ax1.plot(theta_axis_inter, delta_inter, '--', color='orange', label=r'$0.0345x^{2} - 3.7344x + 145.53$')
+ax1.text(55, 51, r'$r^{2} = 0.7709$', fontsize = 10)
+ax1.legend(loc='upper right')
+ax1.grid()
+fig1.tight_layout()
+
+
+plt.show()
